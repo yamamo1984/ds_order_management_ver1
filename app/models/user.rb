efd0659eb21abf48f4c5307b9c14073c7f601854
yamamo1984 <yytworldproducts@gmail.com>
2021-02-13 15:47:47 +0900

    新規登録バリデーションの設置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-         validates :name, presence: true
+  
+         with_options presence: true do
+          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英数字両方を含めてください' }     
+      
+          with_options  format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角文字で入力してください' } do
+          validates :first_name
+          validates :last_name
+        end
+    
+        end 
+  end
+
   has_many :orders 
   has_many :comments       
 end
