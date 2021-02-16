e01c2f52fede9281f9315bcbae52ddc0041169da
yamamo1984 <yytworldproducts@gmail.com>
2021-02-16 17:37:21 +0900

    ユーザーモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   
-         with_options presence: true do
-          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英数字両方を含めてください' }     
-      
-          with_options  format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角文字で入力してください' } do
+        with_options presence: true do
+          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英数字混合にして下さい' }     
+          validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'は半角英数を使用してください' }    
           validates :first_name
           validates :last_name
         end
-    
-        end 
-  end
+        with_options  format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角文字で入力してください' } do
+          validates :first_name
+          validates :last_name
+        end
+     
+
 
   has_many :orders 
   has_many :comments       
