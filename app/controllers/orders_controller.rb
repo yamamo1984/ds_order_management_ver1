4601a30aaba22ccf7add0d590d263c4250dda215
yamamo1984 <yytworldproducts@gmail.com>
2021-02-17 15:38:15 +0900

    注文の編集機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,7 +15,15 @@ class OrdersController < ApplicationController
       render :new
     end   
   end
-  
+
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    order = Order.find(params[:id])
+    order.update(order_params)
+  end  
   
   private
   def order_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "orders#index"
-  resources :orders, only: [:index, :new, :create, :create, :edit, :delete, :show]
-  resources :customers, only: [:index, :new, :create, :edit, :delete, :show]
+  resources :orders, only: [:index, :new, :create, :edit, :update, :delete, :show]
+  resources :customers, only: [:index, :new, :create, :edit, :update, :delete, :show]
 end
