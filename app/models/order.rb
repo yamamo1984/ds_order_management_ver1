128ac860881c4dd11c928e4ec22787dfe723d7a3
yamamo1984 <yytworldproducts@gmail.com>
2021-03-24 18:54:46 +0900

    中止

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,10 +3,6 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :customer
   belongs_to :ship_address
-  has_many :order_items
-  has_many :items, through: :order_items
+  belongs_to :item
   
-
-
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_050203) do
+ActiveRecord::Schema.define(version: 2021_03_24_092703) do
 
   create_table "customers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
@@ -38,15 +38,6 @@ ActiveRecord::Schema.define(version: 2021_03_08_050203) do
     t.string "memo", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "order_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "order_id", null: false
-    t.bigint "item_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_order_items_on_item_id"
-    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -97,8 +88,6 @@ ActiveRecord::Schema.define(version: 2021_03_08_050203) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "order_items", "items"
-  add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
   add_foreign_key "ship_addresses", "customers"
 end
