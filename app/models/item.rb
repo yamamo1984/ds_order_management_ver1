9dd0d4dbff151a1935a6461c6f669ec1d20dbae9
yamamo1984 <yytworldproducts@gmail.com>
2021-05-11 06:42:33 +0900

    Itemの単体テスト実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,15 @@
 class Item < ApplicationRecord
   with_options presence: true do
-    validates :code, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'は半角英数を使用してください' } 
+    validates :code, format: { with: /\A[a-zA-Z0-9-]+\z/, message: 'は半角英数を使用してください' } 
     validates :name
-    validates :price
-    validates :color
-    validates :size
-    validates :stock_num
-  end
-    has_many :orders
+    validates :price, numericality: {only_integer: true, message: "は半角数字を使用してください"}
+    validates :stock_num, numericality: {only_integer: true, message: "は半角数字を使用してください"}
+  end  
+  
+  has_many :orders
 
-    def formatted_name
-      "#{name} | #{color} | #{size} "
-    end  
+  def formatted_name
+    "#{name} | #{color} | #{size} "
+  end  
 
-  end    
+  end   
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :item do
+    code          {"TEST-111"}
+    name          {"TEST商品"}  
+    price         {1}
+    stock_num     {1}
   end
 end
