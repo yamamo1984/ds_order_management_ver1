703a595df43318e949d1d158bb0fc83bb1029a72
yamamo1984 <yytworldproducts@gmail.com>
2021-05-12 07:03:02 +0900

    I18nを導入、エラーメッセージ日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   with_options presence: true do
     validates :code, format: { with: /\A[a-zA-Z0-9-]+\z/, message: 'は半角英数を使用してください' } 
+    validates :code, uniqueness: { message: "は既に登録してあります"}
     validates :name
     validates :price, numericality: {only_integer: true, message: "は半角数字を使用してください"}
     validates :stock_num, numericality: {only_integer: true, message: "は半角数字を使用してください"}
