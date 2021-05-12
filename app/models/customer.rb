a0797b2a7d524724280608fdedc6e0e3da972c90
yamamo1984 <yytworldproducts@gmail.com>
2021-05-13 06:57:38 +0900

    Customerモデルの単体テスト

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,26 +1,20 @@
 class Customer < ApplicationRecord
-   with_options presence: true do
-    validates :email
-    validates :post_code, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'は半角英数を使用してください' }   
-    validates :tel, format: { with: /\A[0-9]+\z/, message: 'は半角文字を使用してください' } 
-    validates :city
-    validates :street_num
-   end
-
-  
-
-   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々０-９]+\z/, message: 'は全角文字を使用してください' } do
-    validates :first_name
-    validates :last_name
-  end 
- 
-
+    with_options presence: true do
+      validates :email, format: { with: /\A\S+@\S+\.\S+\z/, message: 'は半角英数メールアドレス形式での入力必須です' }  
+      validates :post_code, format: { with: /\A\d{7}\z/, message: 'は半角数字７桁を使用してください' }   
+      validates :tel, format: { with: /\A\d{10,11}\z/ , message: 'は半角数字電話番号形式で記入してください' } 
+      validates :company
+      validates :city
+      validates :street_num
+      validates :first_name,  format: { with: /\A[ぁ-んァ-ヶ一-龥々０-９]+\z/, message: 'は全角文字を使用してください' } 
+      validates :last_name,  format: { with: /\A[ぁ-んァ-ヶ一-龥々０-９]+\z/, message: 'は全角文字を使用してください' } 
+    end
 
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :place
 
      #ジャンルの選択が「--」の時は保存できないようにする
-    validates :place_id, numericality: { other_than: 0 } 
+    validates :place_id, numericality: { other_than: 0, message: "を選択してください"}
 
     has_many :orders
     has_many :ship_addresses, dependent: :destroy
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,47 +16,47 @@ RSpec.describe Item, type: :model do
       it "商品コードが空では登録できない" do
         @item.code = ""
         @item.valid?
-        expect(@item.errors.full_messages).to include "Code can't be blank"
+        expect(@item.errors.full_messages).to include "Codeを入力してください"
       end  
       it "商品名が空では登録できない" do
         @item.name = ""
         @item.valid?
-        expect(@item.errors.full_messages).to include "Name can't be blank"
+        expect(@item.errors.full_messages).to include "Nameを入力してください"
       end 
       it "商品価格が空では登録できない" do
         @item.price = ""
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price can't be blank"
+        expect(@item.errors.full_messages).to include "Priceを入力してください"
       end 
       it "商品在庫が空では登録できない" do
         @item.stock_num = ""
         @item.valid?
-        expect(@item.errors.full_messages).to include "Stock num can't be blank"
+        expect(@item.errors.full_messages).to include "Stock numを入力してください"
       end 
       it "商品コードが全角では登録できない" do
         @item.code = "０００"
         @item.valid?
-        expect(@item.errors.full_messages).to include "Code は半角英数を使用してください"
+        expect(@item.errors.full_messages).to include "Codeは半角英数を使用してください"
       end 
       it "商品価格が全角では登録できない" do
         @item.price = "０"
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price は半角数字を使用してください"
+        expect(@item.errors.full_messages).to include "Priceは半角数字を使用してください"
       end 
       it "商品価格に文字があると登録できない" do
         @item.price = "1h"
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price は半角数字を使用してください"
+        expect(@item.errors.full_messages).to include "Priceは半角数字を使用してください"
       end 
       it "商品在庫が全角では登録できない" do
         @item.stock_num = "０"
         @item.valid?
-        expect(@item.errors.full_messages).to include "Stock num は半角数字を使用してください"
+        expect(@item.errors.full_messages).to include "Stock numは半角数字を使用してください"
       end
       it "商品在庫に文字があると登録できない" do
         @item.stock_num = "1h"
         @item.valid?
-        expect(@item.errors.full_messages).to include "Stock num は半角数字を使用してください"
+        expect(@item.errors.full_messages).to include "Stock numは半角数字を使用してください"
       end 
     end  
   end
